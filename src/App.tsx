adba713baf9f886eb65d1ff5cc46ead11528177e
Татьяна Хонякина <hanakinat@gmail.com>
2023-12-05 10:30:24 +0300

    Создана форма для бронирования

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BookingForm} from "./components/BookingForm";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {useTranslation} from "react-i18next";
+import './intell';
+import {Route, Routes} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {QueryParamProvider} from "use-query-params";
+import {ReactRouter6Adapter} from "use-query-params/adapters/react-router-6";
 
 function App() {
-  return (
+    const { i18n } = useTranslation();
+    const queryClient: QueryClient = new QueryClient();
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LocalizationProvider dateAdapter={AdapterDayjs}
+                              adapterLocale={i18n.language}>
+            <QueryClientProvider client={queryClient}>
+                <QueryParamProvider adapter={ReactRouter6Adapter}>
+                    <Routes>
+                        <Route path="/quick-booking"
+                               element={ <BookingForm /> } />
+                    </Routes>
+                </QueryParamProvider>
+            </QueryClientProvider>
+        </LocalizationProvider>
     </div>
   );
 }
